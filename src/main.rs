bb071025b14462093a15f9e804b2f5a1be746c9f
Rasmus Kaj <kaj@kth.se>
2015-09-27 16:33:51 +0200

    Chord boxes for actual used chords.

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io;
 use std::vec::Vec;
+use std::collections::{BTreeSet, BTreeMap};
 
 fn chordbox<'a>(c: &mut Canvas<'a, File>, left: f32, top: f32,
-                name: &str, strings: Vec<i8>)
+                name: &str, strings: &Vec<i8>)
                 -> io::Result<()> {
     let dx = 5.0;
     let dy = 7.0;
@@ -52,6 +53,7 @@ fn chordbox<'a>(c: &mut Canvas<'a, File>, left: f32, top: f32,
     for s in 0..6 {
         let x = left + s as f32 * dx;
         match strings[s+1] {
+            -2 => (), // No-op for unknown chord
             -1 => {
                 let (l, r) = (x-radius, x+radius);
                 let (t, b) = (above-radius, above+radius);
@@ -71,6 +73,22 @@ fn chordbox<'a>(c: &mut Canvas<'a, File>, left: f32, top: f32,
         }
     }
     Ok(())
+}
+
+fn get_known_chords() -> BTreeMap<String, Vec<i8>> {
+    let mut result = BTreeMap::new();
+    {
+        let mut chord = |name: &str, base_fret: i8, e: i8, a: i8, d: i8, g: i8, b: i8, e2: i8| {
+            result.insert(name.to_string(), vec!(base_fret, e, a, d, g, b, e2));
+        };
+        chord("Am",  0,  -1,  0, 2, 2, 1, 0);
+        chord("C",   0,  -1,  3, 2, 0, 1, 0);
+        chord("D",   0,  -1, -1, 0, 2, 3, 2);
+        chord("F",   0,   1,  3, 3, 2, 1, 1);
+        chord("G",   0,   3,  2, 0, 0, 0, 3);
+        chord("Bm7", 2,  -1,  1, 3, 1, 2, 1);
+    }
+    result
 }
 
 enum ChordFileExpression {
@@ -114,6 +132,7 @@ fn main() {
         .unwrap());
     let mut document = Pdf::new(&mut file).unwrap();
     let (width, height) = (596.0, 842.0);
+    let known_chords = get_known_chords();
     document.render_page(width, height, |c| {
         let mut y = height - 30.0;
         let left = 50.0;
@@ -121,6 +140,7 @@ fn main() {
         let times_italic = c.get_font(FontSource::Times_Italic);
         let times = c.get_font(FontSource::Times_Roman);
         let chordfont = c.get_font(FontSource::Helvetica_Oblique);
+        let mut used_chords : BTreeSet<String> = BTreeSet::new();
         for line in source.lines() {
             let token = ChordFileExpression::parse(&line.unwrap());
             try!(match token {
@@ -148,6 +168,7 @@ fn main() {
                     try!(t.pos(left, y));
                     for (i, part) in s.iter().enumerate() {
                         if i % 2 == 1 {
+                            used_chords.insert(part.to_string());
                             try!(t.gsave());
                             try!(t.set_rise(14.0));
                             try!(t.set_font(chordfont, 14.0));
@@ -161,14 +182,16 @@ fn main() {
                 })
             })
         }
-        let x = width - 40.0 * 4.0;
-        try!(chordbox(c, x, 100.0, "Am", vec!(0, -1, 0, 2, 2, 1, 0)));
-        let x = x + 40.0;
-        try!(chordbox(c, x, 100.0, "G", vec!(0, 3, 2, 0, 0, 0, 3)));
-        let x = x + 40.0;
-        try!(chordbox(c, x, 100.0, "D", vec!(0, -1, -1, 0, 2, 3, 2)));
-        let x = x + 40.0;
-        try!(chordbox(c, x, 100.0, "Bm7", vec!(2, -1, 1, 3, 1, 2, 1)));
+        let mut x = width - used_chords.len() as f32 * 40.0;
+        for chord in used_chords.iter() {
+            if let Some(chorddef) = known_chords.get(chord) {
+                try!(chordbox(c, x, 80.0, chord, chorddef));
+            } else {
+                println!("Warning: Unknown chord '{}'.", chord);
+                try!(chordbox(c, x, 80.0, chord, &vec!(0,-2,-2,-2,-2,-2,-2,-2)));
+            }
+            x = x + 40.0;
+        }
         Ok(())
     }).unwrap();
     document.finish().unwrap();
