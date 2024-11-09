90d6f1a4432168aa4479f511cb39c72d370405bb
Rasmus Kaj <rasmus@krats.se>
2024-11-09 15:38:31 +0100

    Improve Nashville handling.
    Handle minor keys, as well as modified offsets (e.g. b7 in a major
    chord).

--- src/key.rs
+++ src/key.rs
@@ -4,12 +4,19 @@ use std::io::Result;
 pub struct Key {
     /// The actual key, A = 0, Bb = 1, B = 2, etc.
     base: u8,
+    /// Is this a major or minor key?
+    maj: bool,
+    /// The name of each tone in the scale from A to G#/Ab
     notes: &'static [&'static str; 12],
 }
 
 impl Key {
     pub fn new(key: &str) -> Result<Key> {
-        let base = match key {
+        let (maj, base) = match key.strip_suffix('m') {
+            Some(base) => (false, base),
+            None => (true, key),
+        };
+        let base = match base {
             "A" => 0,
             "A#" | "Bb" => 1,
             "B" => 2,
@@ -24,33 +31,53 @@ impl Key {
             "G#" | "Ab" => 11,
             _ => todo!("Unkown key {key:?}"),
         };
-        Ok(Key { base, notes: &NOTE[if key.contains('b') { 0 } else { 1 } ] })
+        let noteset = if key.contains('b') || !maj && !key.contains('#') {
+            0
+        } else {
+            1
+        };
+        Ok(Key {
+            base,
+            maj,
+            notes: &NOTE[noteset],
+        })
     }
     pub fn from_nashville(&self, chord: &str) -> String {
-        let (n, m) = parse_nashville(chord);
-        // TODO: Note names differs on key!
-        // An A# in one key is called Bb in another.
+        let (n, m) = parse_nashville(chord, self.maj);
         let note = self.notes[usize::from((self.base + n) % 12)];
         format!("{note}{m}")
     }
 }
 
-fn parse_nashville(chord: &str) -> (u8, &str) {
+fn parse_nashville(chord: &str, maj: bool) -> (u8, &str) {
     let mut chars = chord.chars();
+    let offset = match chars.clone().next() {
+        Some('b') => {
+            chars.next();
+            11
+        }
+        Some('#') => {
+            chars.next();
+            1
+        }
+        _ => 0,
+    };
+
+    #[allow(unused_parens)]
     let num = match chars.next().unwrap_or('?') {
         '1' => 0,
         '2' => 2,
-        '3' => 4,
+        '3' => (if maj { 4 } else { 3 }),
         '4' => 5,
         '5' => 7,
-        '6' => 9,
-        '7' => 10, // TODO: Is a nashville 7 big or small? 10 or 11?
-        c => todo!("Bad nashville chord base {c} in {chord:?}"),
+        '6' => (if maj { 9 } else { 8 }),
+        '7' => (if maj { 11 } else { 10 }),
+        c => panic!("Bad nashville chord base {c} in {chord:?}"),
     };
-    (num, chars.as_str())
+    ((num + offset) % 12, chars.as_str())
 }
 
-static NOTE: [[&str; 12];2] = [
+static NOTE: [[&str; 12]; 2] = [
     [
         "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab",
     ],
